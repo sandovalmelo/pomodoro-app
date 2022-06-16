1ffcbcb6db8604ddac2c23e6db34b99a56553d1c
Sandoval Melo <juniormelo777@gmail.com>
2022-06-16 19:57:28 -0300

    add submit form scripts

--- scripts.js
+++ scripts.js
@@ -1,6 +1,10 @@
 const settingsIcon = document.getElementById("settings-icon");
 const closeIcon = document.getElementById("close-icon");
 const settingsOverlay = document.querySelector(".settings");
+const settingsForm = document.getElementById("settings-form");
+const pomodoroMinutes = document.getElementById("pomodoro-minutes");
+const shortBreak = document.getElementById("short-break");
+const longBreak = document.getElementById("long-break");
 
 settingsIcon.addEventListener("click", () => {
 	document.body.classList.add("settings-open");
@@ -13,4 +17,13 @@ closeIcon.addEventListener("click", () => {
 settingsOverlay.addEventListener("click", (event) => {
 	if (event.target === settingsOverlay)
 		document.body.classList.remove("settings-open");
+});
+
+settingsForm.addEventListener("submit", (event) => {
+	event.preventDefault();
+	console.dir(settingsForm);
+
+	console.log(pomodoroMinutes.value);
+	console.log(shortBreak.value);
+	console.log(longBreak.value);
 });
