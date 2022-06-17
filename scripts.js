0ae6a89ca4c9b62688aa1edaa0976eb1445e0775
Sandoval Melo <juniormelo777@gmail.com>
2022-06-17 17:25:54 -0300

    add pomodoro timer scripts

--- scripts.js
+++ scripts.js
@@ -18,37 +18,69 @@ const closeIcon = document.getElementById("close-icon");
 const settingsOverlay = document.querySelector(".settings");
 const settingsForm = document.getElementById("settings-form");
 
+// Progress bar
+const progressBar = document.querySelector(".progress-bar");
+
 // Time
 const minutesTime = document.querySelector(".time .minutes");
 const secondsTime = document.querySelector(".time .seconds");
 
 let totalTime;
+let timeLeft = totalTime;
+let percentValue = 100;
+
+const radius = progressBar.r.baseVal.value;
+const circunference = radius * 2 * Math.PI;
+
+progressBar.style.strokeDasharray = `${circunference} ${circunference}`;
+progressBar.style.strokeDashoffset = `${circunference}`;
+
+function setProgress(percent) {
+	const offset = circunference - (percent / 100) * circunference;
+	progressBar.style.strokeDashoffset = offset;
+}
+
+setProgress(100);
 
 function renderTime() {
-	const totalValue = totalTime * 60;
-
-	const minutes = Math.floor(totalValue / 60)
+	console.log(totalTime);
+	console.log(timeLeft);
+	const minutes = Math.floor(timeLeft / 60)
 		.toString()
 		.padStart(2, "0");
 
-	const seconds = (totalValue - minutes * 60).toString().padStart(2, "0");
+	const seconds = (timeLeft - minutes * 60).toString().padStart(2, "0");
 
 	minutesTime.innerText = minutes;
 	secondsTime.innerText = seconds;
 }
 
+const interval = setInterval(() => {
+	let percentSeconds = 100 / totalTime;
+	percentValue = percentValue - percentSeconds;
+	setProgress(percentValue);
+	timeLeft--;
+
+	renderTime();
+
+	if (timeLeft <= 0) {
+		setProgress(0);
+		clearInterval(interval);
+	}
+}, 1000);
+
 function setOptionMinutes(option) {
-	console.log(option);
+	setProgress(100);
 
 	if (option === "option-pomodoro") {
-		totalTime = pomodoroMinutes.value;
+		totalTime = Number(pomodoroMinutes.value) * 60;
 	} else if (option === "option-short-break") {
-		totalTime = shortBreak.value;
+		totalTime = Number(shortBreak.value) * 60;
 	} else {
-		totalTime = longBreak.value;
+		totalTime = Number(longBreak.value) * 60;
 	}
 
-	console.log(totalTime);
+	timeLeft = totalTime;
 	renderTime();
 }
 
@@ -85,7 +117,8 @@ pomodoroOptions.addEventListener("change", (event) => {
 });
 
 function getSettingsOptions() {
-	totalTime = pomodoroMinutes.value;
+	totalTime = Number(pomodoroMinutes.value) * 60;
+	timeLeft = totalTime;
 	renderTime();
 }
 
